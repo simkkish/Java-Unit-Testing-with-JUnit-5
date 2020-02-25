bc90871538299f803c47e72b0dcff17bac878196
Kishor Simkhada <ksimkhada@idfbins.com>
2020-02-25 10:05:41 -0700

    Dynamic Tests Using Collection

--- src/test/dynamicTest/DynamicTestDemo.java
+++ src/test/dynamicTest/DynamicTestDemo.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DynamicTestDemo {
@@ -19,5 +24,30 @@ public class DynamicTestDemo {
     @Disabled
     public String testReverseWithString(){
         return "Dynamic Test for reverse method";
+    }
+    @TestFactory
+    public Collection<DynamicTest> dynamicTestsFromCollection(){
+        List<String> inputList=createInputList();
+        List<String> outputList=createOutputList();
+
+        Collection<DynamicTest> dynamicTests=new ArrayList<>();
+
+        for(int i=0;i<inputList.size();i++){
+            String input=inputList.get(i);
+            String output=outputList.get(i);
+            DynamicTest dynamicTest=DynamicTest.dynamicTest("Dynamic Test for reverse() : input - "+input,()->{
+                assertEquals(output,stringHelper.reverse(input));
+            });
+        }
+
+        return dynamicTests;
+    }
+
+    private List<String> createOutputList() {
+        return Arrays.asList("ecar","mom","dad");
+    }
+
+    private List<String> createInputList() {
+        return Arrays.asList("race","mom","dad");
     }
 }
